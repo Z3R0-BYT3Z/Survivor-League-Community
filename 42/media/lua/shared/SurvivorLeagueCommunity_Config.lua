61d87c2c4907dddc36a70416ccf657d7304d95ff
Zero <xkarmarei@gmail.com>
2026-09-02 16:40:56 -0400

    Require the v1.10.3 client-server handshake

--- 42/media/lua/shared/SurvivorLeagueCommunity_Config.lua
+++ 42/media/lua/shared/SurvivorLeagueCommunity_Config.lua
@@ -1,7 +1,7 @@
 SurvivorLeagueCommunity = SurvivorLeagueCommunity or {}
 SurvivorLeagueCommunity.MODULE = "SurvivorLeagueCommunity"
 SurvivorLeagueCommunity.DATA_KEY = "SurvivorLeagueCommunityData"
-SurvivorLeagueCommunity.VERSION = 22
+SurvivorLeagueCommunity.VERSION = 23
 SurvivorLeagueCommunity.PROTOCOL_VERSION = 5
 
 SurvivorLeagueCommunity.THEMES = {
